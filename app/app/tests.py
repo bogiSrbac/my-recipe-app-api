2fa50170545377209b6bcbc8476efb79f75ad296
bogiSrbac <momir.bogosavac@yahoo.com>
2022-10-19 16:43:11 +0200

    postgresql

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+from . import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        res = calc.sub(20-10)
+        self.assertEqual(res, 10)
